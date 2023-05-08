217efecf25801885b2f922107eb0a0132b0fe643
unknown <sobhan13831383yaghobi@gmail.com>
2023-05-08 00:10:22 -0700

    Build new-product.html

---
+++ frontend/panel/main/js/new-product.js
@@ -0,0 +1,24 @@
+import {
+  setEventsForInput,
+  checkInputActive,
+  chekcInputValue,
+} from "../../../js/shared-form.js";
+
+const loginInputs = document.querySelectorAll(".input-box__input");
+const submitNewCourseBtn = document.querySelector("#submit-new-course-btn");
+
+window.addEventListener("keyup", (event) => {
+  if (event.keyCode === 13) {
+    chekcInputValue(loginInputs, doLog);
+  }
+});
+
+submitNewCourseBtn.addEventListener("click", (event) => {
+  chekcInputValue(loginInputs, doLog);
+});
+
+const doLog = () => {
+  console.log("ahaa");
+};
+
+setEventsForInput(loginInputs);
